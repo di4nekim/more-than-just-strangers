c43f032c0a43065d5b6fc832527368d5af3a0cde
Diane <dianekim120@gmail.com>
2025-04-17 23:01:01 -0400

    refactor: user metadata logic to lambdas

---
+++ server/lambdas/user_metadata_handler.js
@@ -0,0 +1,52 @@
+const AWS = require('aws-sdk');
+const cognito = new AWS.CognitoIdentityServiceProvider();
+const dynamodb = new AWS.DynamoDB.DocumentClient();
+
+const USER_POOL_ID = 'eu-north-1';
+const DYNAMODB_TABLE = "UserMetaData";
+
+async function getAllUsers() {
+    const users = [];
+    const response = await cognito.listUsers({ UserPoolId: USER_POOL_ID }).promise();
+
+    response.Users.forEach(user => {
+        const userData = {
+            sub: user.Attributes.find(attr => attr.Name === 'sub').Value,
+            name: user.Attributes.find(attr => attr.Name === 'name')?.Value || 'Unknown',
+            email: user.Attributes.find(attr => attr.Name === 'email')?.Value || 'No Email'
+        };
+        users.push(userData);
+    });
+
+    return users;
+}
+
+async function storeUsersInDynamoDB(users) {
+    const promises = users.map(user => {
+        return dynamodb.put({
+            TableName: DYNAMODB_TABLE,
+            Item: {
+                PK: user.sub,
+                Name: user.name,
+                Email: user.email,
+                IsOnline: false,
+                LastActive: 'Never',
+                IsTyping: false,
+                OnlineStatus: 'Offline',
+                CurrChatPartnerID: '',
+                TTL: Math.floor(Date.now() / 1000) + 3600
+            }
+        }).promise();
+    });
+
+    await Promise.all(promises);
+}
+
+exports.handler = async (event, context) => {
+    const cognitoUsers = await getAllUsers();
+    await storeUsersInDynamoDB(cognitoUsers);
+    return {
+        statusCode: 200,
+        body: 'Users successfully added!'
+    };
+}; 
